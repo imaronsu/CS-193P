a826ad1efc72ec6fa8292ede84cdc3dccc2a69b2
weihsiang <imaronsu@gmail.com>
2013-02-04 19:39:11 -0500

    Modified to match N different cards
    - There are only two matching modes: TWO_CARDS_MATCHING and
    THREE_CARD_MATCHING

--- Matchismo/Matchismo/Model/CardMatchingGame.h
+++ Matchismo/Matchismo/Model/CardMatchingGame.h
@@ -11,13 +11,23 @@
 #import "Deck.h"
 #import "GameTurn.h"
 
+typedef enum gamePlayModeType {
+    TWO_CARDS_MATCHING = 2,
+    THREE_CARDS_MATCHING = 3
+} GamePlayMode;
+
 @interface CardMatchingGame : NSObject
 @property (readonly, nonatomic) NSUInteger score;
 @property (strong, nonatomic) GameTurn *lastFlip;
+@property (nonatomic) GamePlayMode playMode;
 
 // designated initializer
 - (id)initWithCardCount:(NSUInteger)count
               usingDeck:(Deck *)deck;
+
+- (id)initWithCardCount:(NSUInteger)count
+              usingDeck:(Deck *)deck
+           gamePlayMode:(GamePlayMode) mode;
 
 - (void)flipCardAtIndex:(NSUInteger)index;
 - (Card *)cardAtIndex:(NSUInteger)index;
--- Matchismo/Matchismo/Model/CardMatchingGame.m
+++ Matchismo/Matchismo/Model/CardMatchingGame.m
@@ -34,43 +34,65 @@
 - (void)flipCardAtIndex:(NSUInteger)index
 {
     Card *card = [self cardAtIndex:index];
+    NSMutableArray *otherCards = [[NSMutableArray alloc] init];
     self.lastFlip.points = 0;
     
     if (card && !card.isUnplayable) {
         if (!card.isFaceUp) {
+
+            // Gather all of the cards to be matched
+            for (Card *otherCard in self.cards) {
+                if (otherCard.isFaceUp && !otherCard.isUnplayable) {
+                    [otherCards addObject:otherCard];
+                }
+            }
+            
             self.score -= FLIP_COST;
             
             self.lastFlip.state = FLIPPED_UP;
             self.lastFlip.points = -FLIP_COST;
             self.lastFlip.cards = @[card];
             
-            for (Card *otherCard in self.cards) {
-                if (otherCard.isFaceUp && !otherCard.isUnplayable) {
-                    int matchScore = [card match:@[otherCard]];
-                    if (matchScore) {
-                        card.unplayable = YES;
+            if ((otherCards.count+1) == self.playMode) {
+                
+                int matchScore = [card match:otherCards];
+                if (matchScore) {
+                    // we have a match
+                    for (Card *otherCard in otherCards) {
                         otherCard.unplayable = YES;
-                        self.score += matchScore * MATCH_BONUS;
-                        
-                        self.lastFlip.state = MATCH;
-                        self.lastFlip.points += matchScore*MATCH_BONUS;
-                        self.lastFlip.cards = @[card, otherCard];
-                    } else {
-                        otherCard.faceUp = NO;
-                        self.score -= MISMATCH_PENALTY;
-                        
-                        self.lastFlip.state = MISMATCH;
-                        self.lastFlip.points -= MISMATCH_PENALTY;
-                        self.lastFlip.cards = @[card, otherCard];
                     }
-                    break;
+                    card.unplayable = YES;
+                    
+                    self.score += matchScore * MATCH_BONUS;
+                    
+                    self.lastFlip.state = MATCH;
+                    self.lastFlip.points += matchScore*MATCH_BONUS;
+                    
+                    [otherCards addObject:card];
+                    self.lastFlip.cards = [NSArray arrayWithArray:otherCards];
+                } else {
+                    // we don't have a match
+                    
+                    for (Card *otherCard in otherCards) {
+                        otherCard.faceUp = NO;
+                    }
+                    
+                    self.score -= MISMATCH_PENALTY;
+                    
+                    self.lastFlip.state = MISMATCH;
+                    self.lastFlip.points -= MISMATCH_PENALTY;
+                    
+                    [otherCards addObject:card];
+                    self.lastFlip.cards = [NSArray arrayWithArray:otherCards];;
                 }
-            }
+                
+            }       
         } else {
             self.lastFlip.state = FLIPPED_DOWN;
             self.lastFlip.points = 0;
             self.lastFlip.cards = @[card];
         }
+        
         card.faceUp = !card.isFaceUp;
     }
 }
@@ -83,6 +105,13 @@
 
 - (id)initWithCardCount:(NSUInteger)count
               usingDeck:(Deck *)deck
+{
+    return[self initWithCardCount:count usingDeck:deck gamePlayMode:TWO_CARDS_MATCHING];
+}
+
+- (id)initWithCardCount:(NSUInteger)count
+              usingDeck:(Deck *)deck
+           gamePlayMode:(GamePlayMode) mode
 {
     self = [super init];
     
@@ -97,6 +126,8 @@
                 break;
             }
         }
+        
+        self.playMode = mode;
     }
     
     return self;
